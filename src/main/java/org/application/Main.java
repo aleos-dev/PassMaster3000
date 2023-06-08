1c8ab9c56a33c5949e8d499e61fcd1222936d255
Maxym <so.unique.december@gmail.com>
2023-06-08 22:10:49 +0300

    modified code: add new entity

--- src/main/java/org/application/Main.java
+++ src/main/java/org/application/Main.java
@@ -1,19 +1,15 @@
 package org.application;
 
-import org.application.services.JSONService;
-import org.application.objects.password.PasswordObject;
 import org.application.objects.user.User;
+import org.application.services.JSONService;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         User user = new User("Vasya", "vasya@gmail.com");
-        user.setPasswords(new ArrayList<>() {{
-            add(new PasswordObject());
-        }});
-        JSONService service = JSONService.getInstance();
-        service.writeToFile(user);
+        JSONService jsonService = JSONService.service;
+        jsonService.writeToFile(user);
+
     }
 }
---
+++ src/main/java/org/application/objects/website/Credentials.java
@@ -0,0 +1,18 @@
+package org.application.objects.website;
+
+import java.util.Objects;
+
+public record Credentials(String login, String password) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Credentials that)) return false;
+        return Objects.equals(login(), that.login()) && Objects.equals(password(), that.password());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login(), password());
+    }
+}
