7a668ea0da4fb6e24416ce133c7b0369059eb136
Maxym <so.unique.december@gmail.com>
2023-06-09 11:06:57 +0300

    change credentials

--- src/main/java/org/application/objects/website/Credentials.java
+++ src/main/java/org/application/objects/website/Credentials.java
@@ -8,11 +8,11 @@ public record Credentials(String login, String password) {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Credentials that)) return false;
-        return Objects.equals(login(), that.login()) && Objects.equals(password(), that.password());
+        return Objects.equals(login, that.login);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(login(), password());
+        return Objects.hash(login);
     }
 }
