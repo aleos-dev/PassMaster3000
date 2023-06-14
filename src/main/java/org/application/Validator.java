2ac762f12434da44fe188d90e585ffd1d15d8dc5
Aleksei Os <oal.aleos@gmail.com>
2023-06-14 10:28:14 +0300

    Add Validator class to enforce data validation rules and constraints

---
+++ src/main/java/org/application/Validator.java
@@ -0,0 +1,19 @@
+package org.application;
+
+public class Validator {
+
+    public boolean validateLogin(String login) {
+        // Method implementation
+        return false;
+    }
+
+    public boolean validatePassword(String password) {
+        // Method implementation
+        return false;
+    }
+
+    public boolean validateWebsiteName(String website) {
+        // Method implementation
+        return false;
+    }
+}
