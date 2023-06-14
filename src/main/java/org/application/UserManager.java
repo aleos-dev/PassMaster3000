7c97ab033e73bb048871387d67ceef122ace6363
Aleksei Os <oal.aleos@gmail.com>
2023-06-14 10:25:48 +0300

    Add UserManager class, implement authorize method

---
+++ src/main/java/org/application/UserManager.java
@@ -0,0 +1,65 @@
+package org.application;
+
+import org.application.encryption.EncryptionModule;
+import org.application.exception.encryption.DecryptionException;
+import org.application.exception.encryption.InvalidEncryptionKeyException;
+import org.application.objects.user.User;
+import org.application.services.JSONService;
+
+import java.util.Map;
+
+public class UserManager {
+    private final JSONService jService = JSONService.service;
+    private Map<String, User> usersDatabase;
+    private EncryptionModule encryptionModule;
+    private User authorizedUser;
+
+    UserManager() {
+        getUsersDatabase();
+    }
+
+    public boolean createNewUser(String userName, String password) {
+        // Method implementation
+        return false;
+    }
+
+    public void saveUsersDatabase() {
+        // Method implementation
+    }
+
+    private void getUsersDatabase() {
+        // Method implementation
+    }
+
+    public boolean authorize(String userName, String password) {
+        if (!usersDatabase.containsKey(userName)) {
+            return false;
+        }
+
+        try {
+
+            String encryptedPassword = usersDatabase.get(userName).getUserPassword();
+            var encModule = new EncryptionModule(password);
+
+            if (password.equals(encModule.decrypt(encryptedPassword))) {
+                authorizedUser = usersDatabase.get(userName);
+                encryptionModule = encModule;
+                return true;
+            }
+        } catch (InvalidEncryptionKeyException | DecryptionException ignored) {
+            /* ignored */
+        }
+
+        return false;
+
+    }
+
+    public User getUser() {
+        return authorizedUser;
+    }
+
+    public EncryptionModule getEncryptionModule() {
+        return encryptionModule;
+    }
+
+}
